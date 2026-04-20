631305b09c7e391705977c6bd094d3cb2d0df5da
mdriswan1 <mdrriswan20042002@gmail.com>
2026-04-20 11:23:49 +0530

    new user header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,94 +1,79 @@
 import React, { useState } from "react";
-import { HeaderMain, Logo, Menu, MenuToggle } from "../styles/HeaderStyle";
-import { NavLink, useNavigate } from "react-router-dom";
+import { HeaderMain, Menu, MenuToggle } from "../styles/HeaderStyle";
+import { NavLink } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { RegisterButton } from "../styles/SignupStyle";
 import { logout } from "../reducer/authSlice";
 import { useDispatch, useSelector } from "react-redux";
+import NaviButton from "./NavigateButton";
+import UserHeader from "../user/UserHeader";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+
   const dispatch = useDispatch();
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const role = useSelector((state) => state.auth.role);
-  console.log("role",role);
-  
+
   return (
     <HeaderMain>
-      <Logo>
-        
-        <img src="/public/apple-touch-icon.png" width={"50px"} alt="" />
-      </Logo>
+      <div style={{ display: "flex", columnGap: "30px", alignItems: "center" }}>
+        {/* FIX: correct path */}
+        <img src="/apple-touch-icon.png" width="50px" alt="" />
+        <NaviButton />
+      </div>
 
       {isAuthenticated ? (
-        <>
-          <MenuToggle onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? <FaTimes /> : <FaBars />}
-          </MenuToggle>
-{(role==='admin')?
+        role === "admin" ? (
+          <>
+            <MenuToggle onClick={() => setIsOpen(!isOpen)}>
+              {isOpen ? <FaTimes /> : <FaBars />}
+            </MenuToggle>
 
-        <Menu $isOpen={isOpen}>
-          <NavLink to="/adminhome" onClick={() => setIsOpen(false)}>
-            Home
-          </NavLink>
-          <NavLink to="/show-all-topic" onClick={() => setIsOpen(false)}>
-            Manage Question
-          </NavLink>
-          <NavLink to="/create-user" onClick={() => setIsOpen(false)}>
-            Add User
-          </NavLink>
+            <Menu $isOpen={isOpen}>
+              <NavLink to="/adminhome" onClick={() => setIsOpen(false)}>
+                Home
+              </NavLink>
 
- <NavLink to="/creat-exam" onClick={() => setIsOpen(false)}>
-            Create Assessment
-          </NavLink>
-          <NavLink to="/topic-master" onClick={() => setIsOpen(false)}>
-            Topic Master
-          </NavLink>
-          <NavLink to="/add-admin">Add Admin</NavLink>
-          <NavLink to="/" onClick={async (e) => {
-            setIsOpen(false)
-            e.preventDefault();
-            setLoading(true);
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            setLoading(false);
-            dispatch(logout());
+              <NavLink to="/show-all-topic" onClick={() => setIsOpen(false)}>
+                Manage Question
+              </NavLink>
 
-          }}>
-            {loading ? "Signing out..." : "Sign Out"}
-          </NavLink>
+              <NavLink to="/create-user" onClick={() => setIsOpen(false)}>
+                Add User
+              </NavLink>
 
+              <NavLink to="/creat-exam" onClick={() => setIsOpen(false)}>
+                Create Assessment
+              </NavLink>
 
-        </Menu>
-:
-  <Menu>
-      <NavLink to="/userdashboard" onClick={() => setIsOpen(false)}>
-              Home
-      </NavLink>
-     <NavLink to="" onClick={() => setIsOpen(false)}>
-        Manage Question
-      </NavLink>
-      <NavLink to="" onClick={() => setIsOpen(false)}>
-        Add User
-      </NavLink>
-     <NavLink
-              to="/"
-              onClick={async (e) => {
-                setIsOpen(false);
-                e.preventDefault();
-                setLoading(true);
-                await new Promise((resolve) => setTimeout(resolve, 1000));
-                setLoading(false);
-                dispatch(logout());
-              }}
-            >
-              {loading ? "Signing out..." : "Sign Out"}
-        </NavLink>
-  </Menu>
- }
-        </>
+              <NavLink to="/topic-master" onClick={() => setIsOpen(false)}>
+                Topic Master
+              </NavLink>
+
+              <NavLink to="/add-admin" onClick={() => setIsOpen(false)}>
+                Add Admin
+              </NavLink>
+
+              <NavLink
+                to="/"
+                onClick={async (e) => {
+                  e.preventDefault();
+                  setIsOpen(false);
+                  setLoading(true);
+                  await new Promise((resolve) => setTimeout(resolve, 1000));
+                  setLoading(false);
+                  dispatch(logout());
+                }}
+              >
+                {loading ? "Signing out..." : "Sign Out"}
+              </NavLink>
+            </Menu>
+          </>
+        ) : (
+          <UserHeader />
+        )
       ) : null}
-
     </HeaderMain>
   );
 };
--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -3,11 +3,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 import { Toaster } from "sonner";
 import NaviButton from "./NavigateButton";
+import UserHeader from "../user/UserHeader";
 
 function Layout({ children }) {
   return (
     <>
       <Header />
+      {/* <UserHeader/> */}
       <MainContainer>
         {/* <Toaster /> */}
         <LayoutContainer><NaviButton/>{children}</LayoutContainer>
